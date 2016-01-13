d9c68fc1e1874fef20f41a19c8cbfceb92bb7152
Theo Carton <theo@carton.local>
2016-01-13 15:57:23 +0100

    First part of live and let die works

--- Game.elm
+++ Game.elm
@@ -10,13 +10,53 @@ type Action = Tick
 initModel : Int -> Model
 initModel size =
   List.repeat 3 (List.concat (List.repeat size [Empty]))
+
+numberOfNeigbours : Model -> Int -> Int -> Int
+numberOfNeigbours model colIndex rowIndex = 
+  let
+    locationIsNotOwnLocation ownCol ownRow otherCol otherRow =
+      if (ownCol == otherCol) &&
+         (ownRow == otherRow) then
+        False
+      else
+        True
+    
+    neigbours model colIndex rowIndex =
+      List.indexedMap (\cIndex cList ->
+                         List.indexedMap
+                               (\rIndex value ->
+                                  if (cIndex >= colIndex - 1) &&
+                                     (cIndex <= colIndex + 1) &&
+                                     (rIndex >= rowIndex - 1) &&
+                                     (rIndex <= rowIndex + 1) &&
+                                     (value == Alive) &&
+                                     (locationIsNotOwnLocation cIndex rIndex colIndex rowIndex) then
+                                    1
+                                  else
+                                    0
+                               ) cList
+                      ) model
+  in
+    List.sum (List.concat (neigbours model colIndex rowIndex))
   
 update : Action -> Model -> Model
 update action model =
   let
-    emptyIfAlive _ value =
-      Empty
-    emptyAllAlives list =
-      List.indexedMap emptyIfAlive list
+    cellAfterNextTick model colIndex rowIndex state =
+      if state == Alive && numberOfNeigbours model colIndex rowIndex > 1 then
+        Alive
+      else
+        Empty
+      
+    handleSubList model colIndex sublist =
+      List.indexedMap (\rowIndex value ->
+                         cellAfterNextTick model colIndex rowIndex value
+                      ) sublist
   in
-    List.map emptyAllAlives model
+    List.indexedMap (handleSubList model) model
+
+
+
+
+
+
--- Test.elm
+++ Test.elm
@@ -2,7 +2,7 @@
 --import Console
 import Graphics.Element exposing (Element)
 import ElmTest exposing (..)
-import Game exposing (Model, initModel, update, Action)
+import Game exposing (Model, initModel, update, Action, numberOfNeigbours)
 
 emptyWorld : Int -> Game.Model
 emptyWorld size = initModel size
@@ -23,6 +23,29 @@ tests =
                 in
                   (assertEqual (update Game.Tick oneLivingCellModel) (emptyWorld 3))
                )
+        , test "One living cell with one neighbour die in next tick"
+               (
+                let
+                  oneLivingCellModel = [[Game.Empty, Game.Alive, Game.Empty]
+                                       ,[Game.Empty, Game.Alive, Game.Empty]
+                                       ,[Game.Empty, Game.Empty, Game.Empty]]
+                in
+                  (assertEqual (update Game.Tick oneLivingCellModel) (emptyWorld 3))
+               )
+        , test "One living cell with two neighbours live in next tick"
+               (
+                let
+                  before = [[Game.Empty, Game.Empty, Game.Alive]
+                           ,[Game.Empty, Game.Alive, Game.Empty]
+                           ,[Game.Empty, Game.Alive, Game.Empty]]
+                  after = [[Game.Empty, Game.Empty, Game.Empty]
+                          ,[Game.Empty, Game.Alive, Game.Empty]
+                          ,[Game.Empty, Game.Empty, Game.Empty]]
+
+                in
+                  (assertEqual (update Game.Tick before) after)
+               )
+
           
         , test "initModel can init a 3x3 celled world"
                (
@@ -33,6 +56,43 @@ tests =
                 in
                   (assertEqual (emptyWorld 3) expectedWorld)
                )
+         , test "numberOfNeigbours with 1 neigbour"
+                (
+                 let
+                   world = [[Game.Empty, Game.Alive, Game.Empty]
+                           ,[Game.Empty, Game.Empty, Game.Empty]
+                           ,[Game.Empty, Game.Empty, Game.Empty]]
+                 in
+                   (assertEqual (numberOfNeigbours world 1 1) 1)
+                )
+         , test "numberOfNeigbours with 2 neigbours"
+                (
+                 let
+                   world = [[Game.Empty, Game.Alive, Game.Empty]
+                           ,[Game.Empty, Game.Empty, Game.Alive]
+                           ,[Game.Empty, Game.Empty, Game.Empty]]
+                 in
+                   (assertEqual (numberOfNeigbours world 1 1) 2)
+                )
+         , test "numberOfNeigbours with 5 neigbours"
+                (
+                 let
+                   world = [[Game.Alive, Game.Alive, Game.Empty]
+                           ,[Game.Empty, Game.Empty, Game.Alive]
+                           ,[Game.Alive, Game.Empty, Game.Alive]]
+                 in
+                   (assertEqual (numberOfNeigbours world 1 1) 5)
+                )
+         , test "numberOfNeigbours dosen't take own cell into account"
+                (
+                 let
+                   world = [[Game.Empty, Game.Empty, Game.Empty]
+                           ,[Game.Empty, Game.Alive, Game.Empty]
+                           ,[Game.Empty, Game.Empty, Game.Alive]]
+                 in
+                   (assertEqual (numberOfNeigbours world 1 1) 1)
+                )         
+                  
 
         ]
 
